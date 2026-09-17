9ed1a2bfd24fe1021556bab57ad3088434313a20
Ben-theNerd <benkralovec@gmail.com>
2026-09-17 14:43:51 -0400

    Update NeutralTerritory.java

--- NeutralTerritory.java
+++ NeutralTerritory.java
@@ -6,5 +6,23 @@ public class NeutralTerritory extends Territory
   public NeutralTerritory(Country a, boolean mob, String n, Country c)
   {
     super(n, c, false);
+    alignment = a;
+    mobilized = mob;
+  }
+
+  public Country getAlignment()
+  {
+    return alignment;
+  }
+
+  public boolean getMobilizationStatus()
+  {
+    return mobilized;
+  }
+
+  public void mobilize(Country mobilizer) //Mobilizer is the country moving into an unmobilized neutral territory
+  {
+    if(alignment.equals(mobilizer))
+    
   }
 }
