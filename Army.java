7dd3340b3eff46a4926567a0e7ae646994e0bd42
Ben-theNerd <benkralovec@gmail.com>
2026-09-21 17:49:27 -0400

    Update Army.java

--- Army.java
+++ Army.java
@@ -1,1 +1,18 @@
+public class Army
+{
+  private int numInfantry, numArtillery, numPlanes, numTanks;
+  private Country controller;
+  private Territory location;
 
+  public Army(int inf, int art, int pla, int tan, Country cont, Territory loc)
+  {
+    numInfantry = inf;
+    numArtillery = art;
+    numPlanes = pla;
+    numTanks = tan;
+    controller = cont;
+    location = loc;
+  }
+
+  
+}
